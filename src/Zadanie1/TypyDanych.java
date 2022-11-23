c8562b6de83c08ff2e5f111712bbc97f89c0d762
MaOsu0 <mariusz.osuch1@gmail.com>
2022-11-23 20:25:03 +0100

    23.11.2022

--- src/Zadanie1/TypyDanych.java
+++ src/Zadanie1/TypyDanych.java
@@ -11,7 +11,7 @@ class TypyDanych {
         char firstNameChar = 'J';
         char firstSurnameChar = 'K';
 
-        System.out.println("-Cześć Jan, słyszałem, że masz" + yearsOld + "lat i uczysz się programowania. Czy to prawda?");
+        System.out.println("-Cześć Jan, słyszałem, że masz" + " " + yearsOld + " lat i uczysz się programowania. Czy to prawda?");
         System.out.println("-" + isTrue);
         System.out.println("-Czy Twoje inicjały to " + firstNameChar + " " + firstSurnameChar + "?");
         System.out.println("-Tak, " + isTrue);
