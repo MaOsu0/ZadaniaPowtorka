064dd28fc3cabaee22b27c08dd44c819bdb06f96
MaOsu0 <mariusz.osuch1@gmail.com>
2022-11-23 20:23:42 +0100

    23.11.2022

--- src/Zadanie1/TypyDanych.java
+++ src/Zadanie1/TypyDanych.java
@@ -2,9 +2,18 @@ package Zadanie1;
 
 class TypyDanych {
     public static void main(String[] args) {
+        // -Cześć Jan, słyszałem, że masz 25 lat i uczysz się programowania. Czy to prawda?
+        //-true
+        //-Czy Twoje inicjały to J K?
+        //-Tak, true
+        int yearsOld = 25;
+        boolean isTrue = true;
+        char firstNameChar = 'J';
+        char firstSurnameChar = 'K';
 
-        System.out.print("Marek Aureliusz powiedział: \"Nasze życie jest takim, jakim uczyniły je nasze myśli.\"");
-        
-
+        System.out.println("-Cześć Jan, słyszałem, że masz" + yearsOld + "lat i uczysz się programowania. Czy to prawda?");
+        System.out.println("-" + isTrue);
+        System.out.println("-Czy Twoje inicjały to " + firstNameChar + " " + firstSurnameChar + "?");
+        System.out.println("-Tak, " + isTrue);
     }
 }
