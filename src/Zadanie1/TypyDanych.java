4313f09fe10ff30f9a6ca705cb4cd82a0169c825
MaOsu0 <mariusz.osuch1@gmail.com>
2022-11-23 20:17:25 +0100

    23.11.2022

---
+++ src/Zadanie1/TypyDanych.java
@@ -0,0 +1,10 @@
+package Zadanie1;
+
+class TypyDanych {
+    public static void main(String[] args) {
+
+        System.out.print("Marek Aureliusz powiedział: \"Nasze życie jest takim, jakim uczyniły je nasze myśli.\"");
+        
+
+    }
+}
